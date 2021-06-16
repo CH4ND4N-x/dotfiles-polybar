e711627fbd7b8a151a14dc5326aae1dbe4d879e9
CH4ND4N-x <chandanjangid491@gmail.com>
2021-06-16 16:56:53 +0530

    added uptime and memory module , some color changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,8 +10,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#exporting interface for USB port internet
-export DEFAULT_NETWORK_INTERFACE=`ip route | grep '^default' | awk '{print $5}' | head -n 2 | tail -n +2`
+export Eth=`ip route | grep '^default' | awk '{print $5}' | head -n 1 | tail -n +1`
 
 #Launch the bar
 polybar -r -q main -c "$DIR"/config &
